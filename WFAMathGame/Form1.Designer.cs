d15ac0c9cdc48f0ce39676bf3928dce7a1cf0e85
mstfyrdmc <45285832+AdamGiby@users.noreply.github.com>
2020-02-15 19:27:42 +0300

    MathGame

---
+++ WFAMathGame/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace WFAMathGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblSayi1 = new System.Windows.Forms.Label();
+            this.lblOperator = new System.Windows.Forms.Label();
+            this.lblSayi2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSonuc = new System.Windows.Forms.TextBox();
+            this.btnGonder = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblSoruSayisi = new System.Windows.Forms.Label();
+            this.lblDogruSayisi = new System.Windows.Forms.Label();
+            this.lblYanlisSayisi = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lblSeviye = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lblGecenSure = new System.Windows.Forms.Label();
+            this.tmrKronometre = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblSayi1
+            // 
+            this.lblSayi1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSayi1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayi1.Location = new System.Drawing.Point(31, 9);
+            this.lblSayi1.Name = "lblSayi1";
+            this.lblSayi1.Size = new System.Drawing.Size(117, 121);
+            this.lblSayi1.TabIndex = 0;
+            this.lblSayi1.Text = "X";
+            this.lblSayi1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOperator
+            // 
+            this.lblOperator.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOperator.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOperator.Location = new System.Drawing.Point(176, 9);
+            this.lblOperator.Name = "lblOperator";
+            this.lblOperator.Size = new System.Drawing.Size(117, 121);
+            this.lblOperator.TabIndex = 0;
+            this.lblOperator.Text = "+";
+            this.lblOperator.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSayi2
+            // 
+            this.lblSayi2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSayi2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayi2.Location = new System.Drawing.Point(321, 9);
+            this.lblSayi2.Name = "lblSayi2";
+            this.lblSayi2.Size = new System.Drawing.Size(117, 121);
+            this.lblSayi2.TabIndex = 0;
+            this.lblSayi2.Text = "Y";
+            this.lblSayi2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(470, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 121);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "=";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtSonuc
+            // 
+            this.txtSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 49F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSonuc.Location = new System.Drawing.Point(607, 12);
+            this.txtSonuc.Name = "txtSonuc";
+            this.txtSonuc.Size = new System.Drawing.Size(136, 118);
+            this.txtSonuc.TabIndex = 1;
+            // 
+            // btnGonder
+            // 
+            this.btnGonder.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGonder.Location = new System.Drawing.Point(786, 19);
+            this.btnGonder.Name = "btnGonder";
+            this.btnGonder.Size = new System.Drawing.Size(144, 110);
+            this.btnGonder.TabIndex = 2;
+            this.btnGonder.Text = "SEND";
+            this.btnGonder.UseVisualStyleBackColor = true;
+            this.btnGonder.Click += new System.EventHandler(this.btnGonder_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblYanlisSayisi);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.lblGecenSure);
+            this.groupBox1.Controls.Add(this.lblDogruSayisi);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.lblSeviye);
+            this.groupBox1.Controls.Add(this.lblSoruSayisi);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(31, 190);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(899, 222);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "İstatistikler";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(152, 29);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Soru Sayısı:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(32, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(168, 29);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Doğru Sayısı:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(32, 134);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(169, 29);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Yanlış Sayısı:";
+            // 
+            // lblSoruSayisi
+            // 
+            this.lblSoruSayisi.AutoSize = true;
+            this.lblSoruSayisi.Location = new System.Drawing.Point(208, 39);
+            this.lblSoruSayisi.Name = "lblSoruSayisi";
+            this.lblSoruSayisi.Size = new System.Drawing.Size(27, 29);
+            this.lblSoruSayisi.TabIndex = 0;
+            this.lblSoruSayisi.Text = "0";
+            // 
+            // lblDogruSayisi
+            // 
+            this.lblDogruSayisi.AutoSize = true;
+            this.lblDogruSayisi.Location = new System.Drawing.Point(208, 84);
+            this.lblDogruSayisi.Name = "lblDogruSayisi";
+            this.lblDogruSayisi.Size = new System.Drawing.Size(27, 29);
+            this.lblDogruSayisi.TabIndex = 0;
+            this.lblDogruSayisi.Text = "0";
+            // 
+            // lblYanlisSayisi
+            // 
+            this.lblYanlisSayisi.AutoSize = true;
+            this.lblYanlisSayisi.Location = new System.Drawing.Point(208, 134);
+            this.lblYanlisSayisi.Name = "lblYanlisSayisi";
+            this.lblYanlisSayisi.Size = new System.Drawing.Size(27, 29);
+            this.lblYanlisSayisi.TabIndex = 0;
+            this.lblYanlisSayisi.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(571, 39);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(98, 29);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "Seviye:";
+            // 
+            // lblSeviye
+            // 
+            this.lblSeviye.AutoSize = true;
+            this.lblSeviye.Location = new System.Drawing.Point(685, 39);
+            this.lblSeviye.Name = "lblSeviye";
+            this.lblSeviye.Size = new System.Drawing.Size(27, 29);
+            this.lblSeviye.TabIndex = 0;
+            this.lblSeviye.Text = "0";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(571, 84);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(158, 29);
+            this.label13.TabIndex = 0;
+            this.label13.Text = "Geçen Süre:";
+            // 
+            // lblGecenSure
+            // 
+            this.lblGecenSure.AutoSize = true;
+            this.lblGecenSure.Location = new System.Drawing.Point(735, 84);
+            this.lblGecenSure.Name = "lblGecenSure";
+            this.lblGecenSure.Size = new System.Drawing.Size(27, 29);
+            this.lblGecenSure.TabIndex = 0;
+            this.lblGecenSure.Text = "0";
+            // 
+            // tmrKronometre
+            // 
+            this.tmrKronometre.Enabled = true;
+            this.tmrKronometre.Interval = 1000;
+            this.tmrKronometre.Tick += new System.EventHandler(this.tmrKronometre_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnGonder);
+            this.Controls.Add(this.txtSonuc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblSayi2);
+            this.Controls.Add(this.lblOperator);
+            this.Controls.Add(this.lblSayi1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSayi1;
+        private System.Windows.Forms.Label lblOperator;
+        private System.Windows.Forms.Label lblSayi2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSonuc;
+        private System.Windows.Forms.Button btnGonder;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblYanlisSayisi;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblGecenSure;
+        private System.Windows.Forms.Label lblDogruSayisi;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblSeviye;
+        private System.Windows.Forms.Label lblSoruSayisi;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer tmrKronometre;
+    }
+}
+
---
+++ WFAMathGame/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAMathGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        int op,dogruCevapSayisi,yanlisCevapSayisi,soruSayisi;
+        int gecensure = 10;
+        int seviye = 1;
+        
+
+        private void tmrKronometre_Tick(object sender, EventArgs e)
+        {
+            gecensure--;
+            lblGecenSure.Text = gecensure.ToString();
+            if (gecensure<=0)
+            {
+                gecensure = 10;
+                soruSayisi++;
+                lblSoruSayisi.Text = soruSayisi.ToString();
+                lblGecenSure.Text = gecensure.ToString();
+                double sayi1 = rnd.Next(1, 11);
+                lblSayi1.Text = sayi1.ToString();
+                double sayi2 = rnd.Next(1, 11);
+                lblSayi2.Text = sayi2.ToString();
+
+                op = rnd.Next(1, 5);
+                if (op == 1)
+                {
+                    lblOperator.Text = "+";
+                }
+                else if (op == 2)
+                {
+                    lblOperator.Text = "-";
+
+                }
+                else if (op == 3)
+                {
+                    lblOperator.Text = "*";
+                }
+                else
+                {
+                    lblOperator.Text = "/";
+                }
+            }
+            else
+            {
+                gecensure--;
+                lblGecenSure.Text = gecensure.ToString();
+            }
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tmrKronometre.Start();
+            double sayi1 = rnd.Next(1, 11);
+            lblSayi1.Text = sayi1.ToString();
+            double sayi2 = rnd.Next(1, 11);
+            lblSayi2.Text = sayi2.ToString();
+
+            op = rnd.Next(1, 5);
+            if(op==1)
+            {
+                lblOperator.Text = "+";
+            }
+            else if(op==2)
+            {
+                lblOperator.Text = "-";
+
+            }
+            else if(op==3)
+            {
+                lblOperator.Text = "*";
+            }
+            else
+            {
+                lblOperator.Text = "/";
+            }
+            
+        }
+
+        private void btnGonder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double sayi1 = Convert.ToDouble(lblSayi1.Text);
+                double sayi2 = Convert.ToDouble(lblSayi2.Text);
+                double girilensonuc = Convert.ToDouble(txtSonuc.Text);
+                double sonuc = 0;
+                
+                if (op == 1)
+                {
+                    sonuc = sayi1 + sayi2;
+                }
+                else if (op == 2)
+                {
+                    sonuc = sayi1 - sayi2;
+                }
+                else if (op == 3)
+                {
+                    sonuc = sayi1 * sayi2;
+                }
+                else
+                {
+                    sonuc = sayi1 / sayi2;
+                }
+                if (girilensonuc == sonuc)
+                {
+                    dogruCevapSayisi++;
+                    lblDogruSayisi.Text = dogruCevapSayisi.ToString();
+
+                    seviye++;
+                    lblSeviye.Text = seviye.ToString();
+                }
+                else
+                {
+                    yanlisCevapSayisi++;
+                    lblYanlisSayisi.Text = yanlisCevapSayisi.ToString();
+                   
+                }
+                
+                soruSayisi++;
+                lblSoruSayisi.Text = soruSayisi.ToString();
+
+                sayi1 = rnd.Next(1, 11*seviye);
+                lblSayi1.Text = sayi1.ToString();
+                sayi2 = rnd.Next(1, 11*3);
+                lblSayi2.Text = sayi2.ToString();
+
+                op = rnd.Next(1, 5);
+                if (op == 1)
+                {
+                    lblOperator.Text = "+";
+                }
+                else if (op == 2)
+                {
+                    lblOperator.Text = "-";
+
+                }
+                else if (op == 3)
+                {
+                    lblOperator.Text = "*";
+                }
+                else
+                {
+                    lblOperator.Text = "/";
+                }
+                txtSonuc.Text = null;
+                txtSonuc.Focus();
+
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Lütfen Sayısal Değerler Giriniz.");
+            }
+           
+           
+        }
+    }
+}
